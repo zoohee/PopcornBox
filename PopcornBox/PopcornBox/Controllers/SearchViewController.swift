bc0376626089ab5a11f8af5258a2436340bdbe1a
zoohee <jjhh1545@gmail.com>
2023-05-30 19:53:51 +0900

    🪡 UI: 팝콘 로고 이미지 추가

--- PopcornBox/PopcornBox/Controllers/SearchViewController.swift
+++ PopcornBox/PopcornBox/Controllers/SearchViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    var movieListView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    var searchController = UISearchController()
+    var movies: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
     }
-
+    
+    func configure() {
+    }
 }
